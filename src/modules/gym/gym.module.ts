646bc905ea0052a618466cf3ab0f21676ccfbdde
gambitier <csergambitier@gmail.com>
2023-11-26 22:51:05 +0530

    fixes

--- src/modules/gym/gym.module.ts
+++ src/modules/gym/gym.module.ts
@@ -1,11 +1,13 @@
 import { AuthModule } from '@modules/auth/auth.module';
+import { DatabaseErrorHandlerModule } from '@modules/database-error-handler/database.error.handler.module';
 import { PlanController } from '@modules/gym/plan/plan.controller';
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
-  imports: [JwtModule.register({}), AuthModule],
+  imports: [JwtModule.register({}), AuthModule, DatabaseErrorHandlerModule],
   controllers: [PlanController],
-  providers: [],
+  providers: [PrismaService],
 })
 export class GymModule {}
